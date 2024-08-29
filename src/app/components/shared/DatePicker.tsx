b1cac38b0722e3322e4bdb5d0c618a7ff081b631
Chermann-KING <codes.smiling@gmail.com>
2024-08-29 23:18:46 +0200

    🐛 Main: DatePicker > Fixes the problem of retrieving the selected value when the form is sent

--- src/app/components/shared/DatePicker.tsx
+++ src/app/components/shared/DatePicker.tsx
@@ -5,9 +5,13 @@ import ArrowRightIcon from "../../../../public/images/icon-arrow-right.svg";
 
 interface DatePickerProps {
   initialDate?: Date;
+  onDateChange?: (date: Date) => void;
 }
 
-const DatePicker: React.FC<DatePickerProps> = ({ initialDate }) => {
+const DatePicker: React.FC<DatePickerProps> = ({
+  initialDate,
+  onDateChange,
+}) => {
   // état
   const [selectedDate, setSelectedDate] = useState<Date | null>(
     initialDate || null
@@ -31,16 +35,26 @@ const DatePicker: React.FC<DatePickerProps> = ({ initialDate }) => {
   // comportement
   const handleDateClick = (day: number) => {
     const date = new Date(currentYear, currentMonth, day);
-    setSelectedDate(date);
+    const localDate = new Date(
+      date.getTime() - date.getTimezoneOffset() * 60000
+    );
+    setSelectedDate(localDate);
+    console.log("Selected Date: ", localDate);
     setShowCalendar(false);
+    if (onDateChange) {
+      console.log("onDateChange called with: ", localDate);
+      onDateChange(localDate);
+    }
   };
 
   const handlePrevMonth = () => {
     if (currentMonth === 0) {
       setCurrentMonth(11);
       setCurrentYear(currentYear - 1);
+      console.log("Current Year: ", currentYear);
     } else {
       setCurrentMonth(currentMonth - 1);
+      console.log("Current Month: ", currentMonth);
     }
   };
 
@@ -48,26 +62,26 @@ const DatePicker: React.FC<DatePickerProps> = ({ initialDate }) => {
     if (currentMonth === 11) {
       setCurrentMonth(0);
       setCurrentYear(currentYear + 1);
+      console.log("Current Year: ", currentYear);
     } else {
       setCurrentMonth(currentMonth + 1);
+      console.log("Current Month: ", currentMonth);
     }
   };
 
   const formatDate = (date: Date) => {
-    const options = {
-      day: "2-digit",
-      month: "short",
-      year: "numeric",
-    } as const;
+    const options = { day: "2-digit", month: "short", year: "numeric" };
     return date.toLocaleDateString("en-GB", options);
   };
 
-  // Mise à jour si initialDate change
   useEffect(() => {
     if (initialDate) {
-      setSelectedDate(initialDate);
-      setCurrentMonth(initialDate.getMonth());
-      setCurrentYear(initialDate.getFullYear());
+      const localInitialDate = new Date(
+        initialDate.getTime() - initialDate.getTimezoneOffset() * 60000
+      );
+      setSelectedDate(localInitialDate);
+      setCurrentMonth(localInitialDate.getMonth());
+      setCurrentYear(localInitialDate.getFullYear());
     }
   }, [initialDate]);
 
@@ -93,8 +107,9 @@ const DatePicker: React.FC<DatePickerProps> = ({ initialDate }) => {
     <div className="relative" ref={dropdownRef}>
       {/* Dropdown button */}
       <button
+        type="button"
         onClick={() => setShowCalendar(!showCalendar)}
-        className="flex items-center justify-between w-full py-2 pl-5 pr-4 bg-white dark:bg-color03 text-[0.938rem] text-color08 dark:text-white border font-bold border-color05 dark:border-color04 focus:border-color01 dark:focus:border-color01 focus:outline-none rounded-md"
+        className="inline-flex justify-between items-center w-full bg-white dark:bg-color03 border border-color05 dark:border-color04 focus:border-color01 dark:focus:border-color01 focus:outline-none text-[0.938rem] font-bold text-color08 dark:text-white mt-1 pl-5 pr-4 py-2 rounded-md"
       >
         <span>{selectedDate ? formatDate(selectedDate) : "Select Date"}</span>
         <CalendarIcon />
@@ -105,7 +120,7 @@ const DatePicker: React.FC<DatePickerProps> = ({ initialDate }) => {
         <div className="absolute z-10 top-full left-0 mt-2 w-[240px]  bg-white dark:bg-color04 p-4 rounded-md shadow-lg ">
           {/* header box */}
           <div className="flex items-center justify-between mt-2 mb-8">
-            <button onClick={handlePrevMonth}>
+            <button type="button" onClick={handlePrevMonth}>
               <ArrowLeftIcon />
             </button>
             <span className="text-[0.938rem] font-bold text-color08 dark:text-color05">
@@ -114,7 +129,7 @@ const DatePicker: React.FC<DatePickerProps> = ({ initialDate }) => {
                 year: "numeric",
               })}
             </span>
-            <button onClick={handleNextMonth}>
+            <button type="button" onClick={handleNextMonth}>
               <ArrowRightIcon />
             </button>
           </div>
@@ -122,16 +137,17 @@ const DatePicker: React.FC<DatePickerProps> = ({ initialDate }) => {
           <div className="grid grid-cols-7 gap-4">
             {/* Les jours du mois précédent */}
             {Array.from({ length: firstDayOfMonth }).map((_, index) => (
-              <div
+              <button
+                type="button"
                 key={index}
                 className="text-[0.938rem] font-bold text-[#ebebec] dark:text-[#343853] pointer-events-none"
               >
                 {prevMonthDays - firstDayOfMonth + index + 1}
-              </div>
+              </button>
             ))}
             {/* Les jours du mois encours */}
             {Array.from({ length: daysInMonth }, (_, day) => (
-              <button
+              <div
                 key={day}
                 onClick={() => handleDateClick(day + 1)}
                 className={`w-full  text-[0.938rem] hover:text-color01 font-bold flex items-center justify-center outline-none  ${
@@ -142,17 +158,20 @@ const DatePicker: React.FC<DatePickerProps> = ({ initialDate }) => {
                     : "text-color08 dark:text-color05"
                 }   `}
               >
-                <span className="w-full hover:text-color01">{day + 1}</span>
-              </button>
+                <button type="button" className="w-full hover:text-color01">
+                  {day + 1}
+                </button>
+              </div>
             ))}
             {/* Les jours du mois suivant */}
             {Array.from({ length: 6 - lastDayOfMonth }).map((_, index) => (
-              <div
+              <button
+                type="button"
                 key={index}
                 className="text-[0.938rem] font-bold text-[#ebebec] dark:text-[#343853] pointer-events-none"
               >
                 {index + 1}
-              </div>
+              </button>
             ))}
           </div>
         </div>
